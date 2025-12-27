0994bc69f6126b8ebfc2c7ad4787ee0596c8ab4d
NargesGolmohamadi <nargesgol01@gmail.com>
2025-12-27 22:24:49 +0330

    add delete/movie/service

--- app/services/movie_service.py
+++ app/services/movie_service.py
@@ -105,3 +105,11 @@ class MovieService:
             "release_year": movie.release_year,
             "genres": [g.name for g in movie.genres],
         }
+
+    @staticmethod
+    def delete_movie(db: Session, movie_id: int):
+        movie = db.query(Movie).filter(Movie.id == movie_id).first()
+        if not movie:
+            raise HTTPException(status_code=404, detail="Movie not found")
+
+        MovieRepository.delete_movie(db, movie)
